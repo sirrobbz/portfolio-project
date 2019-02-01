82e4eb033fb24dc5cee903087f16ceeb1e2662b3
Robert Mwanzia <rmwanzia@gmail.com>
2019-02-01 15:08:11 +0300

    Added static files & blog detail page

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ from django.db import models
 
 class Blog(models.Model):
 	title = models.CharField(max_length=200)
-	pub_date = models.DateTimeField()
+	pub_date = models.DateField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:150] + '....'
+
+	def pubdate(self):
+		return self.pub_date.strftime('%e %b, %Y')
